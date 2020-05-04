ae6128c51dd78b234e6ef5a7bb52fc4c7ddd3d57
KimSiEon <kimsieon.dev@gmail.com>
2020-05-04 14:56:36 +0900

    Form 태그를 이용한 GET 방식의 요청 처리

---
+++ jsp/src/com/test/jsp/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.test.jsp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login.do")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+	
+	protected void doProc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id"); // 파라미터로 전송된 id를 저장
+		String passwd = request.getParameter("passwd"); // 파라미터로 전송된 passwd를 저장
+		response.setContentType("text/html;charset=utf-8"); // 데이터 타입은 html이고 utf-8 방식으로 문자처리
+		PrintWriter out = response.getWriter(); // 출력 스트림
+		out.println("아이디 : " + id + "<br />");
+		out.println("비밀번호 : " + passwd + "<br />");
+		out.close(); // 스티림은 사용 후 반드시 닫아야 한다.
+	}
+
+}
